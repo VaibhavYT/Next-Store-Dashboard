db22dd474b12b3ad1f918e03c1f90f5a62d1cc7c
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2023-09-13 22:46:11 +0530

    Create Store component is completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
-
-
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ModalProvider } from '@/providers/modal-provider'
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Admin DashBoard',
-  description: 'Admin DashBoard',
-}
+  title: "Admin DashBoard",
+  description: "Admin DashBoard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <ModalProvider/>
-        
-        {children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider />
+
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
